94db406caa544c1b045aa0d38c091da9dbd7d2e4
Matthew Bergin <matthewdbergin@gmail.com>
2023-02-12 13:27:17 -0500

    add button functions

---
+++ logic.js
@@ -0,0 +1,57 @@
+function getComputerChoice() {
+    let num = Math.random() * 100;
+    if(num > 67){
+        console.log("rpck");
+        return "Rock";
+    }
+    else if (num > 33){
+        console.log("paper");
+        return "Paper";
+    }
+    else {
+        console.log("scissors");
+        return "Scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection == "Rock"){
+        switch(computerSelection){
+            case "Rock":
+                return "You Tie!";
+                break;
+            case "Paper":
+                return "You Lose!";
+                break;
+            case "Scissors":
+                return "You Win!";
+                break;
+        }
+    }
+    else if(playerSelection == "Scissors"){
+        switch(computerSelection){
+            case "Rock":
+                return "You Lose!";
+                break;
+            case "Paper":
+                return "You Win!";
+                break;
+            case "Scissors":
+                return "You Tie!";
+                break;
+        }
+    }
+    else if(playerSelection == "Paper"){
+        switch(computerSelection){
+            case "Rock":
+                return "You Win!";
+                break;
+            case "Paper":
+                return "You Tie!";
+                break;
+            case "Scissors":
+                return "You Lose!";
+                break;
+        }
+    }
+}
